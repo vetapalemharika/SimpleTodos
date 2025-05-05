d3b2b6c59988c7c77a60e8a5e2d2189c01899a02
vetapalemharika <vetapalamharika@gmail.com>
2025-05-05 17:49:52 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-    console.log('Database connected');
-  } catch (err) {
-    console.error('Database connection error:', err.message);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
